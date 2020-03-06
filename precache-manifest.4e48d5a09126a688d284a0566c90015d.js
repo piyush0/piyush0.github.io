3e24fc3f9db1a769c9f63fc50524e8601f3cd4c4
Piyush Ajmani <piyush@cuenca.com>
2020-03-06 17:39:06 -0600

    Updates

---
+++ precache-manifest.4e48d5a09126a688d284a0566c90015d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af36aac1ca61d19c3f6a4390c2c3ef03",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4fd98202c735b326f937",
+    "url": "./static/css/main.e2473e87.chunk.css"
+  },
+  {
+    "revision": "2a709564774c939d1185",
+    "url": "./static/js/2.bce850c0.chunk.js"
+  },
+  {
+    "revision": "4fd98202c735b326f937",
+    "url": "./static/js/main.e87065cd.chunk.js"
+  },
+  {
+    "revision": "4f96fbe80315637154e9",
+    "url": "./static/js/runtime-main.5c5cebfe.js"
+  }
+]);
