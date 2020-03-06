8ce4cbbd85d6c67bba7a2e4394bf6b98fdc1b8cf
Piyush Ajmani <piyush@cuenca.com>
2020-03-06 13:10:09 -0600

    Updates

---
+++ precache-manifest.beb5a7ee8982aeab7d5658eb38d24d2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c6dd03feb7288fcd5dc3397d030355c",
+    "url": "/portfolio-create-react-app/index.html"
+  },
+  {
+    "revision": "76e437deecafabf42bf1",
+    "url": "/portfolio-create-react-app/static/css/main.e2473e87.chunk.css"
+  },
+  {
+    "revision": "88429150c6e5bb1c17aa",
+    "url": "/portfolio-create-react-app/static/js/2.85a99219.chunk.js"
+  },
+  {
+    "revision": "76e437deecafabf42bf1",
+    "url": "/portfolio-create-react-app/static/js/main.8d1bc9ad.chunk.js"
+  },
+  {
+    "revision": "7307fc9a229ef17d0c73",
+    "url": "/portfolio-create-react-app/static/js/runtime-main.3bdfb6bc.js"
+  }
+]);
